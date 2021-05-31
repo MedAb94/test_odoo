f5e835b9beefc03faaa493ffb40e393e0bb8cbab
MedAb94 <medab.vall@gmail.com>
2021-05-31 11:41:44 +0000

    first commit

---
+++ __mansifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Medab',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': "Just for testing",
+    'depends': [],
+    'data': [],
+    'website': 'https://www.odoo.com/page/community-builder',
+}
