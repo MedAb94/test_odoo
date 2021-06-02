96e6b001f0e9a726b5b8994cbc1e260e49d37c53
MedAb94 <medab.vall@gmail.com>
2021-06-02 17:04:44 +0000

    home work

--- models/da.py
+++ models/da.py
@@ -4,18 +4,26 @@ from odoo import fields, models
 class Da(models.Model):
     _name = 'medab.da'
 
-    name = fields.Char(string='Ref',required=True)
+    name = fields.Char(string='Ref', required=True)
 
-    demandeur  = fields.Many2one(comodel_name='res.users',string='Demandeur',required=False)
+    demandeur = fields.Many2one(comodel_name='res.users', string='Demandeur', required=False)
 
-    description = fields.Char()
+    description = fields.Text()
 
-    departement  = fields.Many2one(comodel_name='hr.department',string='Departement',required=False,retaled='demandeur.department_id')
+    note = fields.Text()
 
-    date  = fields.Date(string='Data',required=False)
+    status = fields.Boolean(string="Statut", default=False)
 
-    da_ids= fields.One2many(
+    departement = fields.Many2one(comodel_name='hr.department', string='Departement', required=False,
+                                  retaled='demandeur.department_id')
+
+    date = fields.Date(string='Date', required=False)
+
+    da_ids = fields.One2many(
         comodel_name='medab.da_detail',
         inverse_name='da_id',
         string='Details',
         required=False)
+
+    def validate_da(self):
+        self.status = True
